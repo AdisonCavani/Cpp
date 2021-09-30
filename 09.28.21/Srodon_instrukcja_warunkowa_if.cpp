91e45da2a538bdb058ee89aa42defea06f76d2ac
Adison Cavani <adrian.srodon@gmail.com>
2021-10-01 01:09:56 +0200

    Sync

---
+++ 09.28.21/Srodon_instrukcja_warunkowa_if.cpp
@@ -0,0 +1,24 @@
+// Środoń Adrian
+// Instrukcja warunkowa if
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+
+    cout << "Podaj 3 liczby:\n";
+    cin >> a >> b >> c;
+
+    if (a > b)
+        if(a > c)
+            cout << a << " max";
+        else
+            cout << c << " max";
+    else if(c > b)
+        cout << c << " max";
+    else
+        cout << b << " max";
+
+    return 0;
+}
